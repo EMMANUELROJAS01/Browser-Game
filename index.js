92607bb70b99dff889817f46096b450a858f6a7b
EMMANUELROJAS01 <392rojas@gmail.com>
2022-10-18 21:59:22 -0700

    changed font size on navbar

---
+++ index.js
@@ -0,0 +1,14 @@
+class Player{
+    constructor() {
+        this.position = {
+            x: 200, 
+            y: 200
+        }
+        this.velocity = {
+            x : 0
+        }
+        const img = document.querySelector("img"); 
+        img.src = "./img/Spaceship.jpg";
+    }
+
+        }
